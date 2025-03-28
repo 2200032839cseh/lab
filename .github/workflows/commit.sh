e90a91ba682721ca502a0970cc2e4c3c160c9d62
2200032839cseh <2200032839cseh@gmail.com>
2025-03-28 12:22:28 +0530

    Update commit.sh

--- .github/workflows/commit.sh
+++ .github/workflows/commit.sh
@@ -1,1 +1,12 @@
 
+#!/bin/bash
+
+DIRECTORY_TO_WATCH="."  # Change this to your directory path if needed
+COMMIT_MESSAGE="Auto-commit: Changes detected"
+
+inotifywait -m -r -e modify,create,delete,move "$DIRECTORY_TO_WATCH" |
+while read -r path action file; do
+    echo "Detected $action in $file. Running git add and commit."
+    git add .
+    git commit -m "$COMMIT_MESSAGE"
+done
